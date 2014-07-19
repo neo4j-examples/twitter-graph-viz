68f1ca29132117430ab64018c2b2d4fd17219bfa
Michael Hunger <github@jexp.de>
2014-07-18 22:38:02 -0700

    initial

---
+++ run.py
@@ -0,0 +1,22 @@
+import web
+import neo
+from neo import count_relationships
+
+render = web.template.render("templates/")
+urls = (
+    '/', 'index',
+    '/page', 'page'
+)
+
+class index:
+    def GET(self):
+        return "Hello, world!"
+
+class page:
+    def GET(self):
+        name = count_relationships()
+        return render.page(name)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
